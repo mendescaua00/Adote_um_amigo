4e94546e658273aaecb003957ec29360160bcfc4
mendescaua <mendescaua00@gmail.com>
2025-10-27 11:59:19 -0300

    V1.12.18

---
+++ js/data.js
@@ -0,0 +1,70 @@
+{
+    "id": 1,
+    "nome": "Luna",
+    "especie": "Gato",
+    "idade": "Filhote",
+    "porte": "Pequeno",
+    "sexo": "Fêmea",
+    "descricao": "Brincalhona e carinhosa.",
+    "foto": "https://p2.trrsf.com/image/fget/cf/1200/1600/middle/images.terra.com/2023/12/17/866513462-gato.jpg"
+  },
+  {
+    "id": 2,
+    "nome": "Thor",
+    "especie": "Cachorro",
+    "idade": "Adulto",
+    "porte": "Médio",
+    "sexo": "Macho",
+    "descricao": "Companheiro, adora passeios.",
+    "foto": "https://images.unsplash.com/photo-1568572933382-74d440642117?q=80&w=1200&auto=format&fit=crop"
+  },
+  {
+    "id": 3,
+    "nome": "Mimi",
+    "especie": "Gato",
+    "idade": "Adulto",
+    "porte": "Pequeno",
+    "sexo": "Fêmea",
+    "descricao": "Calma e adora colo.",
+    "foto": "https://images.unsplash.com/photo-1543852786-1cf6624b9987?q=80&w=1200&auto=format&fit=crop"
+  },
+  {
+    "id": 4,
+    "nome": "Bento",
+    "especie": "Cachorro",
+    "idade": "Filhote",
+    "porte": "Pequeno",
+    "sexo": "Macho",
+    "descricao": "Esperto e obediente.",
+    "foto": "https://images.unsplash.com/photo-1543466835-00a7907e9de1?q=80&w=1200&auto=format&fit=crop"
+  },
+  {
+    "id": 5,
+    "nome": "Nina",
+    "especie": "Cachorro",
+    "idade": "Filhote",
+    "porte": "Médio",
+    "sexo": "Fêmea",
+    "descricao": "Tranquila, ideal para apartamento.",
+     "foto": "https://images.unsplash.com/photo-1510337550647-e84f83e341ca?q=80&w=1200&auto=format&fit=crop"
+  },
+  {
+    "id": 6,
+    "nome": "Zeus",
+    "especie": "Cachorro",
+    "idade": "Adulto",
+    "porte": "Grande",
+    "sexo": "Macho",
+    "descricao": "Protetor e dócil.",
+    "foto": "https://static.nationalgeographicbrasil.com/files/styles/image_3200/public/01-domesticated-dog.webp?w=760&h=507"
+  },
+  {
+    "id": 7,
+    "nome": "Amora",
+    "especie": "Gato",
+    "idade": "Filhote",
+    "porte": "Pequeno",
+    "sexo": "Fêmea",
+    "descricao": "Curiosa e brincalhona.",
+    "foto": "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?q=80&w=1200&auto=format&fit=crop"
+  },
