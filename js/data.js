71f41048d99fb63db69ddb6bb9f68ca7ee0bffe0
mendescaua <mendescaua00@gmail.com>
2025-10-27 12:00:08 -0300

    V1.12.19

--- js/data.js
+++ js/data.js
@@ -68,3 +68,63 @@
     "descricao": "Curiosa e brincalhona.",
     "foto": "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?q=80&w=1200&auto=format&fit=crop"
   },
+  {
+    "id": 8,
+    "nome": "Bob",
+    "especie": "Cachorro",
+    "idade": "Adulto",
+    "porte": "Médio",
+    "sexo": "Macho",
+    "descricao": "Dócil com crianças.",
+    "foto": "https://images.unsplash.com/photo-1552053831-71594a27632d?q=80&w=1200&auto=format&fit=crop"
+  },
+  {
+    "id": 9,
+    "nome": "Mel",
+    "especie": "Gato",
+    "idade": "Adulto",
+    "porte": "Pequeno",
+    "sexo": "Fêmea",
+    "descricao": "Acolhedora e elegante.",
+    "foto": "https://inovaveterinaria.com.br/wp-content/uploads/2016/01/Gato-siames-INOVA-scaled.jpg"
+  },
+  {
+    "id": 10,
+    "nome": "Rex",
+    "especie": "Cachorro",
+    "idade": "Adulto",
+    "porte": "Grande",
+    "sexo": "Macho",
+    "descricao": "Energia alta, ama correr.",
+    "foto": "https://blog-static.petlove.com.br/wp-content/uploads/2020/10/puppy-1-petlove.jpg"
+  },
+  {
+    "id": 11,
+    "nome": "Bela",
+    "especie": "Cachorro",
+    "idade": "Filhote",
+    "porte": "Pequeno",
+    "sexo": "Fêmea",
+    "descricao": "Latidinho fofo.",
+    "foto": "https://conteudo.imguol.com.br/c/entretenimento/eb/2022/03/23/cachorro-da-raca-lulu-da-pomeramia-1648065976007_v2_3x4.jpg"
+  },
+  {
+    "id": 12,
+    "nome": "Fred",
+    "especie": "Cachorro",
+    "idade": "Adulto",
+    "porte": "Médio",
+    "sexo": "Macho",
+    "descricao": "Gosta de brincar de bola.",
+    "foto": "https://jpimg.com.br/uploads/2024/11/8-racas-de-cachorros-mais-carinhosas-do-mundo.jpg"
+  },
+  {
+    "id": 13,
+    "nome": "Jade",
+    "especie": "Gato",
+    "idade": "Adulto",
+    "porte": "Pequeno",
+    "sexo": "Fêmea",
+    "descricao": "Boazinha e silenciosa.",
+    "foto": "https://admin.cnnbrasil.com.br/wp-content/uploads/sites/12/2025/05/gato-laranja-e1748043537291.jpg?w=1200&h=1200&crop=1"
+  },
